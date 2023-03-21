ba44216e312d00aa8c0212d894b47ab36b3cfca7
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-21 18:04:00 +0530

    transition events on red block

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
+// ReactDOMClient.createRoot(<App />, document.getElementById('root'));
 registerServiceWorker();
