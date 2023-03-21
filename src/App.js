3fab00d8ed85f2b8589f381690753aa7ba5c3d46
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-03-21 16:33:31 +0530

    transition componenet applied on simple div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from 'react-transition-group/Transition'
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -29,12 +29,25 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        {this.state.showBlock ? <div style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          margin: 'auto'
-            }}></div> : null}
+        <Transition 
+          in={this.state.showBlock} 
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          >
+          { state => (
+              <div 
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition:'opacity 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1,
+                }}>
+            </div>
+          ) }
+        </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.closeModel} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModel}>Open Modal</button>
